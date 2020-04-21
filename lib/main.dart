c2feae697625cb5076f649412548ca6348b7d009
AleWasser <alejowasser@gmail.com>
2020-04-21 17:09:49 -0300

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Weather App',
+      theme: ThemeData(
+        primaryColor: Colors.purple,
+      ),
+      home: MyAppHome(),
+    );
+  }
+}
+
+class MyAppHome extends StatefulWidget {
+  @override
+  _MyAppHomeState createState() => _MyAppHomeState();
+}
+
+class _MyAppHomeState extends State<MyAppHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Hello world!'),
+      ),
+    );
+  }
+}
